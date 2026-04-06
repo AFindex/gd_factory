8a728d6d1949a7b32881f28e5aa15b4981fd9b9e
AFindex <1151516766@qq.com>
2026-04-06 19:17:00 +0800

    1

--- scripts/factory/FactoryUiTheme.cs
+++ scripts/factory/FactoryUiTheme.cs
@@ -14,7 +14,10 @@ public static class FactoryUiTheme
     public static readonly Color SurfaceRaised = new(0.10f, 0.10f, 0.10f, 0.98f);
     public static readonly Color SurfaceInset = new(0.04f, 0.04f, 0.04f, 0.92f);
     public static readonly Color SurfaceOverlay = new(0.02f, 0.02f, 0.02f, 0.94f);
+    public static readonly Color SurfaceHover = new(0.16f, 0.16f, 0.16f, 0.98f);
     public static readonly Color SurfaceInverse = new("F2F2F2");
+    public static readonly Color SurfaceSelected = new("D8D8D8");
+    public static readonly Color SurfaceSelectedHover = new("C6C6C6");
     public static readonly Color BorderStrong = new("FAFAFA");
     public static readonly Color Border = new("B7B7B7");
     public static readonly Color BorderMuted = new("6A6A6A");
@@ -24,6 +27,7 @@ public static class FactoryUiTheme
     public static readonly Color TextSubtle = new("A0A0A0");
     public static readonly Color TextFaint = new("767676");
     public static readonly Color TextInverse = new("080808");
+    public static readonly Color TextContrast = new("020202");
     public static readonly Color StatusOk = new("F5F5F5");
     public static readonly Color StatusWarn = new("DADADA");
     public static readonly Color StatusError = new("8A8A8A");
@@ -108,22 +112,42 @@ public static class FactoryUiTheme
         return style;
     }
 
+    public static StyleBoxFlat CreateTabStyle(Color backgroundColor, Color borderColor, int borderWidth = 1)
+    {
+        var style = CreatePanelStyle(backgroundColor, borderColor, borderWidth);
+        style.ContentMarginLeft = 12;
+        style.ContentMarginRight = 12;
+        style.ContentMarginTop = 7;
+        style.ContentMarginBottom = 7;
+        return style;
+    }
+
     public static void ApplyButtonTheme(BaseButton button, bool compact = false, bool invertPressed = true)
     {
         var horizontalPadding = compact ? 10 : 12;
         var verticalPadding = compact ? 6 : 8;
-        button.AddThemeStyleboxOverride("normal", CreateButtonStyle(SurfaceBase, Border, 1, horizontalPadding, verticalPadding));
-        button.AddThemeStyleboxOverride("hover", CreateButtonStyle(SurfaceRaised, BorderStrong, 1, horizontalPadding, verticalPadding));
+        button.AddThemeStyleboxOverride("normal", CreateButtonStyle(SurfaceInset, BorderMuted, 1, horizontalPadding, verticalPadding));
+        button.AddThemeStyleboxOverride("hover", CreateButtonStyle(SurfaceHover, BorderStrong, 1, horizontalPadding, verticalPadding));
         button.AddThemeStyleboxOverride(
             "pressed",
             CreateButtonStyle(invertPressed ? SurfaceInverse : SurfaceInset, BorderStrong, invertPressed ? 2 : 1, horizontalPadding, verticalPadding));
-        button.AddThemeStyleboxOverride("focus", CreateButtonStyle(SurfaceRaised, BorderStrong, 2, horizontalPadding, verticalPadding));
+        button.AddThemeStyleboxOverride(
+            "hover_pressed",
+            CreateButtonStyle(invertPressed ? SurfaceSelected : SurfaceHover, BorderStrong, 2, horizontalPadding, verticalPadding));
+        button.AddThemeStyleboxOverride("focus", CreateButtonStyle(invertPressed ? SurfaceInverse : SurfaceHover, BorderStrong, 2, horizontalPadding, verticalPadding));
         button.AddThemeStyleboxOverride("disabled", CreateButtonStyle(SurfaceInset, BorderMuted, 1, horizontalPadding, verticalPadding));
         button.AddThemeColorOverride("font_color", TextMuted);
         button.AddThemeColorOverride("font_hover_color", Text);
-        button.AddThemeColorOverride("font_focus_color", Text);
+        button.AddThemeColorOverride("font_focus_color", invertPressed ? TextInverse : Text);
         button.AddThemeColorOverride("font_pressed_color", invertPressed ? TextInverse : Text);
+        button.AddThemeColorOverride("font_hover_pressed_color", invertPressed ? TextContrast : Text);
         button.AddThemeColorOverride("font_disabled_color", TextFaint);
+        button.AddThemeColorOverride("icon_normal_color", TextMuted);
+        button.AddThemeColorOverride("icon_hover_color", Text);
+        button.AddThemeColorOverride("icon_focus_color", invertPressed ? TextInverse : Text);
+        button.AddThemeColorOverride("icon_pressed_color", invertPressed ? TextInverse : Text);
+        button.AddThemeColorOverride("icon_hover_pressed_color", invertPressed ? TextContrast : Text);
+        button.AddThemeColorOverride("icon_disabled_color", TextFaint);
     }
 
     public static void ApplyLineEditTheme(LineEdit lineEdit)
@@ -139,6 +163,14 @@ public static class FactoryUiTheme
     public static void ApplyItemListTheme(ItemList list)
     {
         list.AddThemeStyleboxOverride("panel", CreatePanelStyle(SurfaceInset, BorderMuted, 1, RadiusNone, 4));
+        list.AddThemeStyleboxOverride("hovered", CreatePanelStyle(SurfaceHover, BorderStrong, 1, RadiusNone, 2));
+        list.AddThemeStyleboxOverride("selected", CreatePanelStyle(SurfaceInverse, BorderStrong, 2, RadiusNone, 2));
+        list.AddThemeStyleboxOverride("selected_focus", CreatePanelStyle(SurfaceInverse, BorderStrong, 2, RadiusNone, 2));
+        list.AddThemeStyleboxOverride("hovered_selected", CreatePanelStyle(SurfaceInverse, BorderStrong, 2, RadiusNone, 2));
+        list.AddThemeStyleboxOverride("hovered_selected_focus", CreatePanelStyle(SurfaceInverse, BorderStrong, 2, RadiusNone, 2));
+        list.AddThemeStyleboxOverride("cursor", CreateOutlineStyle(BorderStrong, borderWidth: 2, contentMargin: 2));
+        list.AddThemeStyleboxOverride("cursor_unfocused", CreateOutlineStyle(Border, borderWidth: 1, contentMargin: 2));
+        list.AddThemeStyleboxOverride("focus", CreateOutlineStyle(BorderStrong, borderWidth: 2, contentMargin: 1));
         list.AddThemeColorOverride("font_color", TextMuted);
         list.AddThemeColorOverride("font_hovered_color", Text);
         list.AddThemeColorOverride("font_selected_color", TextInverse);
@@ -148,9 +180,22 @@ public static class FactoryUiTheme
 
     public static void ApplyTabContainerTheme(TabContainer tabs)
     {
-        tabs.AddThemeColorOverride("font_selected_color", TextInverse);
+        tabs.AddThemeStyleboxOverride("panel", CreatePanelStyle(SurfaceOverlay, BorderSoft, 1));
+        tabs.AddThemeStyleboxOverride("tabbar_background", CreatePanelStyle(SurfaceInset, BorderMuted, 1));
+        tabs.AddThemeStyleboxOverride("tab_unselected", CreateTabStyle(SurfaceInset, BorderMuted, 1));
+        tabs.AddThemeStyleboxOverride("tab_hovered", CreateTabStyle(SurfaceSelectedHover, BorderStrong, 2));
+        tabs.AddThemeStyleboxOverride("tab_selected", CreateTabStyle(SurfaceSelected, BorderStrong, 2));
+        tabs.AddThemeStyleboxOverride("tab_focus", CreateTabStyle(SurfaceSelectedHover, BorderStrong, 2));
+        tabs.AddThemeStyleboxOverride("tab_disabled", CreateTabStyle(SurfaceInset, BorderMuted, 1));
+        tabs.AddThemeColorOverride("font_selected_color", TextContrast);
         tabs.AddThemeColorOverride("font_unselected_color", TextMuted);
-        tabs.AddThemeColorOverride("font_hovered_color", Text);
+        tabs.AddThemeColorOverride("font_hovered_color", TextContrast);
+        tabs.AddThemeColorOverride("font_disabled_color", TextFaint);
+        tabs.AddThemeColorOverride("icon_selected_color", TextContrast);
+        tabs.AddThemeColorOverride("icon_hovered_color", TextContrast);
+        tabs.AddThemeColorOverride("icon_unselected_color", TextMuted);
+        tabs.AddThemeColorOverride("icon_disabled_color", TextFaint);
+        tabs.AddThemeConstantOverride("outline_size", 0);
     }
 
     public static void ApplyProgressBarTheme(ProgressBar bar)
